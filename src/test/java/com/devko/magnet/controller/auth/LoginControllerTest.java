696e913205c454dabda241137cf670ff118115a2
HoYoon-Lee <happylhy0630@naver.com>
2022-01-16 00:35:04 +0900

    테스트 코드 작성

--- src/test/java/com/devko/magnet/controller/auth/LoginControllerTest.java
+++ src/test/java/com/devko/magnet/controller/auth/LoginControllerTest.java
@@ -1,24 +1,29 @@
-package com.devko.magnet;
+package com.devko.magnet.controller.auth;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.restdocs.RestDocumentationContextProvider;
 import org.springframework.restdocs.RestDocumentationExtension;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.context.WebApplicationContext;
 
+import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @Transactional
 @ExtendWith({RestDocumentationExtension.class})
-class MagnetApplicationTests {
+class LoginControllerTest {
     @Autowired
     private WebApplicationContext context;
 
@@ -26,10 +31,30 @@ class MagnetApplicationTests {
 
     @BeforeEach
     public void setUp(RestDocumentationContextProvider restDocumentation) {
-        this.mockMvc = MockMvcBuilders.webAppContextSetup(context)
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.context)
                 .apply(documentationConfiguration(restDocumentation))
                 .build();
     }
 
+    @Test
+    void login() {
 
+    }
+
+    @Test
+    void logout() {
+
+    }
+
+    @Test
+    void saveAdditionalInfo() throws Exception {
+        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+        params.add("job", "collegian");
+        params.add("field", "backend");
+        params.add("career", "0");
+        mockMvc.perform(MockMvcRequestBuilders.post("/auth/info/2")
+                .params(params)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
 }
