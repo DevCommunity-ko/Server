aee57e2786863486f0085e7f8ffd2e3df921a191
HoYoon-Lee <happylhy0630@naver.com>
2022-03-13 20:40:37 +0900

    Feat: 팀 등록 API, 수정 API 작성

--- src/main/java/com/devko/magnet/controller/team/TeamController.java
+++ src/main/java/com/devko/magnet/controller/team/TeamController.java
@@ -1,10 +1,33 @@
 package com.devko.magnet.controller.team;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import com.devko.magnet.dto.team.TeamInfo;
+import com.devko.magnet.model.entity.Team;
+import com.devko.magnet.service.team.TeamService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
-@RequestMapping("/team")
+import java.util.HashMap;
+import java.util.Map;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("team")
 public class TeamController {
+    private final TeamService teamService;
 
+    @PostMapping("{leaderId}")
+    public ResponseEntity<Map<String, Object>> createTeam(@PathVariable("leaderId") Long leaderId, @RequestBody TeamInfo teamInfo){
+        Map<String, Object> result = new HashMap<>();
+        long id = teamService.createTeam(leaderId, teamInfo);
+        result.put("teamId", id);
+        return new ResponseEntity<>(result, HttpStatus.CREATED);
+    }
+
+    @PatchMapping("{teamId}")
+    public ResponseEntity<Team> editTeam(@PathVariable("teamId") Long teamId, @RequestBody TeamInfo teamInfo){
+        Team team = teamService.editTeam(teamId, teamInfo);
+        return new ResponseEntity<>(team, HttpStatus.OK);
+    }
 }
