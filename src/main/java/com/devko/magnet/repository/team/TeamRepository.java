71e573474517de3e9d1e302c1466a283e15c3a89
HoYoon-Lee <happylhy0630@naver.com>
2022-03-28 00:39:27 +0900

    Feat: TeamRepository에 DTO를 반환하는 함수 작성

--- src/main/java/com/devko/magnet/repository/team/TeamRepository.java
+++ src/main/java/com/devko/magnet/repository/team/TeamRepository.java
@@ -1,8 +1,24 @@
 package com.devko.magnet.repository.team;
 
+import com.devko.magnet.dto.team.TeamDetail;
+import com.devko.magnet.dto.team.TeamListItem;
 import com.devko.magnet.model.entity.Team;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface TeamRepository extends JpaRepository<Team, Long> {
     boolean existsByName(String name);
+
+    @Query("SELECT new com.devko.magnet.dto.team.TeamListItem(t.id, t.name, t.profileURL) " +
+            "FROM Team as t")
+    List<TeamListItem> getTeamList();
+
+    @Query("SELECT new com.devko.magnet.dto.team.TeamDetail(t.name, t.profileURL, t.introduction, t.githubURL, t.isPublic, t.leader)" +
+            "FROM Team as t " +
+            "WHERE t.id = :teamId")
+    Optional<TeamDetail> findTeamDetailById(@Param("teamId") Long teamId);
 }
