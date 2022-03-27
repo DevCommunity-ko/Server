1193e63437e5ec0599c1b9114c2e846354185c94
HoYoon-Lee <happylhy0630@naver.com>
2022-03-28 00:46:54 +0900

    Feat: 팀 목록 조회, 상세 조회, 삭제, 공개 여부 변경 API 작성

--- src/main/java/com/devko/magnet/controller/team/TeamController.java
+++ src/main/java/com/devko/magnet/controller/team/TeamController.java
@@ -1,33 +1,61 @@
 package com.devko.magnet.controller.team;
 
+import com.devko.magnet.dto.team.TeamDetail;
 import com.devko.magnet.dto.team.TeamInfo;
-import com.devko.magnet.model.entity.Team;
+import com.devko.magnet.dto.team.TeamListItem;
+import com.devko.magnet.model.Message;
 import com.devko.magnet.service.team.TeamService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+
+import static com.devko.magnet.exception.StatusCode.*;
 
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("team")
+@RequestMapping("teams")
 public class TeamController {
     private final TeamService teamService;
 
     @PostMapping("{leaderId}")
-    public ResponseEntity<Map<String, Object>> createTeam(@PathVariable("leaderId") Long leaderId, @RequestBody TeamInfo teamInfo){
+    public ResponseEntity<Message> createTeam(@PathVariable("leaderId") Long leaderId, @RequestBody TeamInfo teamInfo){
         Map<String, Object> result = new HashMap<>();
         long id = teamService.createTeam(leaderId, teamInfo);
         result.put("teamId", id);
-        return new ResponseEntity<>(result, HttpStatus.CREATED);
+        return new Message(CREATE_NEW, result).toResponseEntity();
+    }
+
+    @PutMapping("{teamId}")
+    public ResponseEntity<Message> editTeam(@PathVariable("teamId") Long teamId, @RequestBody TeamInfo teamInfo){
+        TeamDetail team = teamService.editTeam(teamId, teamInfo);
+        return new Message(SUCCESS, team).toResponseEntity();
+    }
+
+    @GetMapping
+    public ResponseEntity<Message> getTeamList(){
+        List<TeamListItem> teamList = teamService.getTeamList();
+        return new Message(SUCCESS, teamList).toResponseEntity();
+    }
+
+    @GetMapping("{teamId}")
+    public ResponseEntity<Message> getTeam(@PathVariable("teamId") Long teamId){
+        TeamDetail team = teamService.getTeam(teamId);
+        return new Message(SUCCESS, team).toResponseEntity();
+    }
+
+    @DeleteMapping("{teamId}")
+    public ResponseEntity<Message> deleteTeam(@PathVariable("teamId") Long teamId){
+        teamService.deleteTeam(teamId);
+        return new Message(DELETE).toResponseEntity();
     }
 
     @PatchMapping("{teamId}")
-    public ResponseEntity<Team> editTeam(@PathVariable("teamId") Long teamId, @RequestBody TeamInfo teamInfo){
-        Team team = teamService.editTeam(teamId, teamInfo);
-        return new ResponseEntity<>(team, HttpStatus.OK);
+    public ResponseEntity<Message> changeTeamPublicOption(@PathVariable("teamId") Long teamId, @RequestBody Map<String, Boolean> param){
+        TeamDetail team = teamService.changeTeamPublicOption(teamId, param.get("isPossible"));
+        return new Message(SUCCESS, team).toResponseEntity();
     }
 }
--- src/main/java/com/devko/magnet/service/team/TeamService.java
+++ src/main/java/com/devko/magnet/service/team/TeamService.java
@@ -1,17 +1,22 @@
 package com.devko.magnet.service.team;
 
+import com.devko.magnet.dto.team.TeamDetail;
 import com.devko.magnet.dto.team.TeamInfo;
+import com.devko.magnet.dto.team.TeamListItem;
 import com.devko.magnet.exception.CustomException;
-import com.devko.magnet.exception.ErrorCode;
+import com.devko.magnet.exception.StatusCode;
 import com.devko.magnet.model.entity.Team;
 import com.devko.magnet.model.entity.User;
 import com.devko.magnet.repository.team.TeamRepository;
 import com.devko.magnet.repository.user.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
+
+import static com.devko.magnet.exception.StatusCode.*;
 
 @Service
 @RequiredArgsConstructor
@@ -21,23 +26,48 @@ public class TeamService {
 
     public long createTeam(long leaderId, TeamInfo teamInfo){
         if(teamRepository.existsByName(teamInfo.getName()))
-            throw new CustomException(ErrorCode.DUPLICATE_TEAM);
+            throw new CustomException(DUPLICATE_TEAM);
         Optional<User> leader = userRepository.findById(leaderId);
         if(leader.isEmpty())
-            throw new CustomException(ErrorCode.MEMBER_NOT_FOUND);
+            throw new CustomException(MEMBER_NOT_FOUND);
         Team team = new Team(teamInfo);
         team.setLeader(leader.get());
         team = teamRepository.save(team);
         return team.getId();
     }
 
-    public Team editTeam(long teamId, TeamInfo teamInfo){
+    @Transactional
+    public TeamDetail editTeam(long teamId, TeamInfo teamInfo){
         Optional<Team> team = teamRepository.findById(teamId);
         if(team.isEmpty())
-            throw new CustomException(ErrorCode.TEAM_NOT_FOUND);
+            throw new CustomException(TEAM_NOT_FOUND);
         if(!teamInfo.getName().equals(team.get().getName()) && teamRepository.existsByName(teamInfo.getName()))
-            throw new CustomException(ErrorCode.DUPLICATE_TEAM);
+            throw new CustomException(DUPLICATE_TEAM);
         team.get().editTeam(teamInfo);
-        return teamRepository.save(team.get());
+        return new TeamDetail(team.get());
+    }
+
+    public List<TeamListItem> getTeamList(){
+        return teamRepository.getTeamList();
+    }
+
+    public TeamDetail getTeam(Long teamId){
+        Optional<TeamDetail> teamDetail = teamRepository.findTeamDetailById(teamId);
+        if(teamDetail.isEmpty())
+            throw new CustomException(TEAM_NOT_FOUND);
+        return teamDetail.get();
+    }
+
+    public void deleteTeam(Long teamId){
+        teamRepository.deleteById(teamId);
+    }
+
+    @Transactional
+    public TeamDetail changeTeamPublicOption(Long teamId, boolean isPublic){
+        Optional<Team> team = teamRepository.findById(teamId);
+        if(team.isEmpty())
+            throw new CustomException(TEAM_NOT_FOUND);
+        team.get().setPublic(isPublic);
+        return new TeamDetail(team.get());
     }
 }
